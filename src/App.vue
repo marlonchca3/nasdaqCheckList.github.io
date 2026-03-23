dcb21d6d8d6dec28ffa1939df92deb1835fc242d
marlonchca3 <marlonchca3@gmail.com>
2026-03-23 14:47:05 -0500

    qwe

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
-<script>
-import NasdaqFiboChecklist from "./components/NasdaqFiboChecklist.vue";
-
-export default {
-  components: {
-    NasdaqFiboChecklist
-  }
-};
-</script>
-
 <template>
-  <NasdaqFiboChecklist />
+  <div style="padding: 40px; color: white; background: #111; min-height: 100vh;">
+    <h1>Prueba Vue</h1>
+    <p>Si ves esto, Vue sí está cargando.</p>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
